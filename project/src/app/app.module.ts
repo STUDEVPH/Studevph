a9244652fc0848c3a962ea548d7e78cecbc5df4c
Erman Louie Faminiano <koresensi.299@gmail.com>
2023-07-01 17:53:35 +0800

    Added router module

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
