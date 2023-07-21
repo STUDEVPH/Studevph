2b1cd0730c5feae5067b3206d6b68efc7252d3d4
jerick <jericksignapan3331@gmail.com>
2023-07-21 21:19:41 +0800

    refactor: :fire: remove angular material

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,36 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule} from './app-routing.module';
-import { MatSidenavModule } from '@angular/material/sidenav'; 
-import { SideNavComponent } from './components/sidenav/sidenav.component';
-import { TabsComponent } from './components/tabs/tabs.component';
-import { ContentEventsComponent } from './components/content-events/content-events.component';
-import { ContentSeminarsComponent } from './components/content-seminars/content-seminars.component';
-import { ContentAboutUsComponent } from './components/content-about-us/content-about-us.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-
-
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatSlideToggleModule,
-    AppRoutingModule,
-    SideNavComponent,
-    TabsComponent,
-    ContentAboutUsComponent,
-    ContentEventsComponent,
-    ContentSeminarsComponent,
-    NavbarComponent
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
