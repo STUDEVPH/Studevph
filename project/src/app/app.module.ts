4beed5ac0eadd65660347e5847936bf5d9853108
ditosoft-ianlabicani <labicaniian@gmail.com>
2023-07-30 11:53:00 +0800

    content related components to content folder

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule} from './app-routing.module';
-import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { AppRoutingModule } from './app-routing.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { SideNavComponent } from './sidenav/sidenav.component';
 import { TabsComponent } from './tabs/tabs.component';
-import { ContentEventsComponent } from './content-events/content-events.component';
-import { ContentSeminarsComponent } from './content-seminars/content-seminars.component';
-import { ContentAboutUsComponent } from './content-about-us/content-about-us.component';
+import { ContentAboutUsComponent } from './content/content-about-us/content-about-us.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { ContentSeminarsComponent } from './content/content-seminars/content-seminars.component';
+import { ContentEventsComponent } from './content/content-events/content-events.component';
 
 @NgModule({
-  declarations: [
-    AppComponent 
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -24,12 +21,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     AppRoutingModule,
     SideNavComponent,
     TabsComponent,
-    ContentAboutUsComponent,
-    ContentEventsComponent,
-    ContentSeminarsComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
